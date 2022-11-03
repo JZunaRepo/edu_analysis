8b5973f03084289dc28c8137a1d4e4c3805f0488
Bingbong0201 <jonnathan.zunalargo@gmail.com>
2022-11-02 20:58:58 -0400

    Almost done

--- analysis.py
+++ analysis.py
@@ -1,8 +1,26 @@
 import csv
+
+from numpy import number
 list_data = []
 with open("states_all.csv", "r") as infile:
     # load in data as DictReader
     reader = csv.DictReader(infile)
     for row in reader:
         list_data.append(row)
-print(list_data)
+#Total rows in list_date
+num_rows = 0
+for num in list_data:
+  num_rows += 1
+print(num_rows)
+
+#Filtering rows with NEW_YORK as STATE
+state_data = [row for row in list_data if row["STATE"] == "NEW_YORK"]
+
+#Filtering rows with values in column "AVG_MATH_4_SCORE"
+avg_score = [row for row in state_data if row["AVG_MATH_4_SCORE"] != ""]
+
+#Counting how many rows have AVG_MATH_4_SCORE values 
+rows_in_avg = 0
+for num in avg_score:
+  rows_in_avg += 1
+print(rows_in_avg)
